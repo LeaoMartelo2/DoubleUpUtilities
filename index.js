bc86ccfdbf41dd91e4f3e150be2fbe2c4a65c1bb
LeaoMartelo <leaomartelo@gmail.com>
2025-12-31 20:44:19 -0300

    cleanup

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-function chat_msg(msg) {
-    ChatLib.chat("&4[&bDoubleUpUtilities&4]&r " + msg);
-}
+import { chat_msg } from "./shared/chat_utils"
 
-function build_text_component(name, murd_or_detc) {
-
-    let expose_cmd;
-
-    if (murd_or_detc === "murderer")  {expose_cmd = "/expose_murderer";  }
-
-    if (murd_or_detc === "detective") {expose_cmd = "/expose_detective"; }
-
-    expose_cmd += " " + name;
-
-    let tmp_msg = "&lClick here to &4EXPOSE&r&l your fellow " + murd_or_detc;
-
-    
-    let msg = new TextComponent({
-        text: tmp_msg,
-        clickEvent: {
-            action: "run_command",
-            value: expose_cmd
-        },
-        hoverEvent: {
-            action: "show_text",
-            value: "Click here to EXPOSE them"
-        }
-    });
-
-    return msg;
-
-}
-
-/////////////////////////////////////
-
-register("command", function(name) {
-    if (name == undefined) {
-        chat_msg("Name at /expose_murderer must not be empty or undefined.");
-        return;
-    }
-    ChatLib.say(name + " is Murderer, i saw chat");
-}).setName("expose_murderer");
+require("./features/expose")
 
 
+/*
 
-register("command", function(name) {
-    if(name == undefined) {
-        chat_msg("Name at /expose_detective must not be empty or undefined.");
-        return;
-    }
-    ChatLib.say(name + " is Detective, i saw chat");
-}).setName("expose_detective");
+One of the Murderers, Sipkyy, was killed.
 
-/////////////////////////////////////
+A Bow has been dropped!
 
-register("chat", (murderer, event) => {
-
-    let msg = build_text_component(murderer, "murderer");
-    ChatLib.chat(msg);
-
-}).setCriteria("Your fellow Murderer is: ${murderer}").setContains();
-
-
-register("chat", (detective, event) => {
-
-    let msg = build_text_component(detective, "detective");
-    ChatLib.chat(msg);
-
-}).setCriteria("Your fellow Detective is: ${detective}").setContains();
-
-
+*/
---
+++ shared/chat_utils.js
@@ -0,0 +1,4 @@
+export function chat_msg(msg) {
+    ChatLib.chat("&4[&bDoubleUpUtilities&4]&r " + msg);
+}
+
