fc2a559fbe872d0058aa12645dc16bef9629c48a
LeaoMartelo <leaomartelo@gmail.com>
2025-12-31 19:51:36 -0300

    initial release

--- README.md
+++
@@ -1,5 +0,0 @@
-# CTJS template module for 1.21+
-
-## Replace this with some info about your module
-
-### add stuff here
--- index.js
+++ index.js
@@ -1,3 +1,71 @@
-register('command', () => {
-    ChatLib.chat("template_module command ran");
-}).setName('template_module');
+function chat_msg(msg) {
+    ChatLib.chat("&4[&bDoubleUpUtilities&4]&r " + msg);
+}
+
+function build_text_component(name, murd_or_detc) {
+
+    let expose_cmd;
+
+    if (murd_or_detc === "murderer")  {expose_cmd = "/expose_murderer";  }
+
+    if (murd_or_detc === "detective") {expose_cmd = "/expose_detective"; }
+
+    expose_cmd += " " + name;
+
+    let tmp_msg = "&lClick here to &4EXPOSE&r&l your fellow " + murd_or_detc;
+
+    
+    let msg = new TextComponent({
+        text: tmp_msg,
+        clickEvent: {
+            action: "run_command",
+            value: expose_cmd
+        },
+        hoverEvent: {
+            action: "show_text",
+            value: "Click here to EXPOSE them"
+        }
+    });
+
+    return msg;
+
+}
+
+/////////////////////////////////////
+
+register("command", function(name) {
+    if (name == undefined) {
+        chat_msg("Name at /expose_murderer must not be empty or undefined.");
+        return;
+    }
+    ChatLib.say(name + " is Murderer, i saw chat");
+}).setName("expose_murderer");
+
+
+
+register("command", function(name) {
+    if(name == undefined) {
+        chat_msg("Name at /expose_detective must not be empty or undefined.");
+        return;
+    }
+    ChatLib.say(name + " is Detective, i saw chat");
+}).setName("expose_detective");
+
+/////////////////////////////////////
+
+register("chat", (murderer, event) => {
+
+    let msg = build_text_component(murderer, "murderer");
+    ChatLib.chat(msg);
+
+}).setCriteria("Your fellow Murderer is: ${murderer}").setContains();
+
+
+register("chat", (detective, event) => {
+
+    let msg = build_text_component(detective, "detective");
+    ChatLib.chat(msg);
+
+}).setCriteria("Your fellow Detective is: ${detective}").setContains();
+
+
